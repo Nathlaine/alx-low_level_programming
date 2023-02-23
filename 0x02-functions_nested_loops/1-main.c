503a1a49472ead9be22469c6fab463d861945f7c
Nathlaine <nathlainematilda@gmail.com>
2023-02-23 13:37:33 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Reurn: Always 0.
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+julien@ubuntu:~/0x02$  gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
+julien@ubuntu:~/0x02$ ./0-putchar
+_putchar
+julien@ubuntu:~/0x02$
